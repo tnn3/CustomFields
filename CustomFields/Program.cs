22b95cf26ef490c98adfe9e82e6fb686fca5322f
Tõnn Vaher <tonnvaher@gmail.com>
2018-01-23 23:34:20 +0200

    Seed users

--- CustomFields/Program.cs
+++ CustomFields/Program.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using DAL;
+using DAL.Extensions;
+using Domain;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace WebApplication
 {
@@ -7,7 +14,26 @@ namespace WebApplication
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    DbInitializer.Seed(context, roleManager, userManager).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Seeding the database failed.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
--- DAL/Extensions/DbInitializer.cs
+++ DAL/Extensions/DbInitializer.cs
@@ -1,30 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Domain;
-using Interfaces;
 using Microsoft.AspNetCore.Identity;
 
 namespace DAL.Extensions
 {
     public class DbInitializer
     {
-        private readonly ApplicationDbContext _context;
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
-
-        public DbInitializer(ApplicationDbContext context,
-            UserManager<ApplicationUser> userManager,
-            RoleManager<IdentityRole> roleManager)
+        public static async Task Seed(ApplicationDbContext context, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
-            _context = context;
-            _userManager = userManager;
-            _roleManager = roleManager;
+            context.Database.EnsureCreated();
+
+            if (!context.Users.Any())
+            {
+                await SeedUsers(roleManager, userManager);
+            }
         }
 
-        public void Seed()
+        private static async Task SeedUsers(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
-            
+            const string roleAdmin = "admin";
+            const string roleUser = "user";
+            const string password = "Testing1";
+
+            await roleManager.CreateAsync(new IdentityRole(roleAdmin));
+            var appUser = new ApplicationUser
+            {
+                UserName = "Admin",
+                Email = "admin@test.ee"
+            };
+            await userManager.CreateAsync(appUser, password);
+            await userManager.AddToRoleAsync(appUser, roleAdmin);
+
+            await roleManager.CreateAsync(new IdentityRole(roleUser));
+            appUser = new ApplicationUser
+            {
+                UserName = "User",
+                Email = "user@test.ee"
+            };
+            await userManager.CreateAsync(appUser, password);
+            await userManager.AddToRoleAsync(appUser, roleUser);
         }
     }
 }
