f8545232047522541a53f781403235384e222564
Tõnn Vaher <tonnvaher@gmail.com>
2018-03-23 17:33:22 +0200

    Customfield hide fields by type

--- CustomFields/wwwroot/js/site.js
+++ CustomFields/wwwroot/js/site.js
@@ -1,1 +1,18 @@
-﻿// Write your JavaScript code.
+﻿"use strict";
+$(document).ready(function () {
+    $("#FieldType").change(function () {
+        $(".hideable").addClass("hidden");
+        const selectedValue = $("#FieldType").val();
+        switch (selectedValue) {
+            case "0":
+            case "6":
+                $("#field-lengths").removeClass("hidden");
+                break;
+            case "1":
+            case "2":
+            case "3":
+                $("#field-values").removeClass("hidden");
+                break;
+        }
+    });
+});
