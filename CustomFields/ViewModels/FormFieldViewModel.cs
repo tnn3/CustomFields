a390a86873ffbcadaf2308a05752d4f490b65b4b
Tõnn Vaher <tonnvaher@gmail.com>
2019-04-25 11:21:49 +0300

    Fieldtype selected value refactoring

--- CustomFields/ViewModels/FormFieldViewModel.cs
+++ CustomFields/ViewModels/FormFieldViewModel.cs
@@ -6,5 +6,6 @@ namespace CustomFields.ViewModels
     {
         public ICustomField CustomField { get; set; }
         public string Start { get; set; }
+        public string SelectedValue => CustomField.CombinedFields != null ? CustomField.CombinedFields[0].FieldValue : string.Empty;
     }
 }
