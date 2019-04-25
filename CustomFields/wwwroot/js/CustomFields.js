c73fdebe450e35379599fc3835856fa3463577f8
Tõnn Vaher <tonnvaher@gmail.com>
2019-04-25 11:34:04 +0300

    Fix checkbox option showing hidden fields on customfield create

--- CustomFields/wwwroot/js/CustomFields.js
+++ CustomFields/wwwroot/js/CustomFields.js
@@ -1,16 +1,16 @@
 ﻿"use strict";
 
-document.getElementById("CustomField_FieldType").addEventListener('change', function (event) {
+document.getElementById("CustomField_FieldType").addEventListener("change", function (event) {
     changeFieldVisibility(event.target.value);
 });
 
 function changeFieldVisibility(selectedValue) {
+    document.getElementById("field-values").classList.add("hidden");
+    document.getElementById("text-fields").classList.add("hidden");
     if (selectedValueIsOfTextFieldType(selectedValue)) {
         document.getElementById("text-fields").classList.remove("hidden");
-        document.getElementById("field-values").classList.add("hidden");
     } else if (selectedValueIsOfSelectionFieldType(selectedValue)) {
         document.getElementById("field-values").classList.remove("hidden");
-        document.getElementById("text-fields").classList.add("hidden");
     }
 }
 
